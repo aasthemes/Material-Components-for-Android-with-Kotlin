2dfa0e151783864dc6a7dece1f08107103d48fd6
Wanz <wawanrandianto1@gmail.com>
2018-11-11 22:30:34 +0700

    test button 1

---
+++ app/src/main/java/edu/stts/materialcomponents/activity/menu13/BasicActivity.kt
@@ -0,0 +1,14 @@
+package edu.stts.materialcomponents.activity.menu13
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import edu.stts.materialcomponents.R
+
+class BasicActivity: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_menu_13_basic)
+
+
+    }
+}
--- app/src/main/java/edu/stts/materialcomponents/activity/menu13/Menu13Activity.kt
+++ app/src/main/java/edu/stts/materialcomponents/activity/menu13/Menu13Activity.kt
@@ -2,7 +2,9 @@ package edu.stts.materialcomponents.activity.menu13
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.widget.Toast
 import edu.stts.materialcomponents.R
+import kotlinx.android.synthetic.main.activity_menu_13.*
 
 class Menu13Activity: AppCompatActivity() {
 
@@ -13,6 +15,9 @@ class Menu13Activity: AppCompatActivity() {
     }
 
     private fun init() {
+        button1.setOnClickListener {
+            Toast.makeText(this, "This is a Toast Message", Toast.LENGTH_SHORT).show()
+        }
     }
 
 }
