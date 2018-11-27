6a7158dbc98b92cf513a081c4c268993ff51c1be
Deny Wenk <denywenk160196@gmail.com>
2018-11-27 20:34:44 +0700

    first commit, coba toast

--- app/src/main/java/edu/stts/materialcomponents/activity/menu10/Menu10Activity.kt
+++ app/src/main/java/edu/stts/materialcomponents/activity/menu10/Menu10Activity.kt
@@ -3,13 +3,23 @@ package edu.stts.materialcomponents.activity.menu10
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import edu.stts.materialcomponents.R
+import javax.swing.text.View
+import java.awt.Button
+
+
 
 class Menu10Activity: AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_menu_10)
-        init()
+        setContentView(R.layout.activity_main)
+        val showToast = findViewById(R.id.showToast) as Button
+        showToast.setOnClickListener(object : View.OnClickListener() {
+            fun onClick(v: View) {
+                Toast.makeText(this@MainActivity, "Toast Success", Toast.LENGTH_SHORT).show()
+            }
+        })
+
     }
 
     private fun init() {
