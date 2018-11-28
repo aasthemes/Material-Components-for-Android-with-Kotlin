3b9233e5b4354a893f26d8afcf3e658b7320c6ff
Yohanes Wisnu Perdana <perdanayohanes43@gmail.com>
2018-11-28 18:47:07 +0700

    Menu team 12

---
+++ app/src/main/java/edu/stts/materialcomponents/activity/menu12/ChangeColorActivity.kt
@@ -0,0 +1,17 @@
+package edu.stts.materialcomponents.activity.menu12
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import edu.stts.materialcomponents.R
+
+class ChangeColorActivity:AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_menu_12_change_color)
+        init()
+    }
+
+    private fun init() {
+
+    }
+}
