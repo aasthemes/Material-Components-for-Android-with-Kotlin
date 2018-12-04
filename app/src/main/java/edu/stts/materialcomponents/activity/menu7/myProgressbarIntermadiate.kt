876392a85d8e15bc70b748fd0bfd007255ce432e
wawan <wawanrandianto1@gmail.com>
2018-12-04 20:15:22 +0700

    Indeterminate Progress Bar...

---
+++ app/src/main/java/edu/stts/materialcomponents/activity/menu7/myProgressbarIntermadiate.kt
@@ -0,0 +1,49 @@
+package edu.stts.materialcomponents.activity.menu7
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ProgressBar
+import edu.stts.materialcomponents.R
+import kotlinx.android.synthetic.main.activity_my_progressbar_intermadiate.*
+
+class myProgressbarIntermadiate : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_my_progressbar_intermadiate)
+
+        // get the references from layout file
+        val btnStartProgress = this.button123
+        val progressBar: ProgressBar = this.progressBar123
+
+        // when button is clicked, start the task
+        btnStartProgress.setOnClickListener { v ->
+
+            // task is run on a thread
+            Thread(Runnable {
+                // dummy thread mimicking some operation whose progress cannot be tracked
+
+                // display the indefinite progressbar
+                this@myProgressbarIntermadiate.runOnUiThread(java.lang.Runnable {
+                    progressBar.visibility = android.view.View.VISIBLE
+                })
+
+                // performing some dummy time taking operation
+                try {
+                    var i=0;
+                    while(i<Int.MAX_VALUE){
+                        i++
+                    }
+                } catch (e: InterruptedException) {
+                    e.printStackTrace()
+                }
+
+                // when the task is completed, make progressBar gone
+                this@myProgressbarIntermadiate.runOnUiThread(java.lang.Runnable {
+                    progressBar.visibility = android.view.View.GONE
+                })
+            }).start()
+        }
+
+    }
+}
