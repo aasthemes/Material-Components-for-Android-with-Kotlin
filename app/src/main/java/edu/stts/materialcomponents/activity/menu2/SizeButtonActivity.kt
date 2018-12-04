0f1a2d088a95b6b88eef5c8c15a1f401fae749ad
master <hendrawijay13@gmail.com>
2018-12-04 20:12:24 +0700

    added more activities

---
+++ app/src/main/java/edu/stts/materialcomponents/activity/menu2/SizeButtonActivity.kt
@@ -0,0 +1,13 @@
+package edu.stts.materialcomponents.activity.menu2
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import edu.stts.materialcomponents.R
+
+class SizeButtonActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_menu_2_size_button)
+    }
+}
