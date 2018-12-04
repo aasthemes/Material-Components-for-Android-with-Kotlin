75ecb2ecd57949e1819541a8c880c6fd19969173
Cornelius FN <javablackrose@gmail.com>
2018-12-04 20:29:40 +0700

    basic FAB with custom position

---
+++ app/src/main/java/edu/stts/materialcomponents/activity/menu17/BasicWithCustomPosition.kt
@@ -0,0 +1,13 @@
+package edu.stts.materialcomponents.activity.menu17
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import edu.stts.materialcomponents.R
+
+class BasicWithCustomPosition : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_menu_17_basic_with_custom_position)
+    }
+}
