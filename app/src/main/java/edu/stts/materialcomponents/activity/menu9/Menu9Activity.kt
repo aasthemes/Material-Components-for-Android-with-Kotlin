2e756319771d1f3295c4cfe2d92e76c5ba4eb12f
Aloysius Heinrich <yuliusrizky007@gmail.com>
2018-12-04 20:49:44 +0700

    Update Bottom Navigation

--- app/src/main/java/edu/stts/materialcomponents/activity/menu9/Menu9Activity.kt
+++ app/src/main/java/edu/stts/materialcomponents/activity/menu9/Menu9Activity.kt
@@ -1,8 +1,10 @@
 package edu.stts.materialcomponents.activity.menu9
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import edu.stts.materialcomponents.R
+import kotlinx.android.synthetic.main.activity_menu_9.*
 
 class Menu9Activity: AppCompatActivity() {
 
@@ -10,9 +12,14 @@ class Menu9Activity: AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_menu_9)
         init()
+
+
     }
 
     private fun init() {
+        button1.setOnClickListener {
+            setContentView(R.layout.activity_menu_9_style)
+        }
     }
 
 }
