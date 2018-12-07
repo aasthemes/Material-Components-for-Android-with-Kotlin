a4fcba1ae6251a0fd31623d493241bc2756709bc
Jansen Theodorus <jansentheodorus@gmail.com>
2018-12-07 13:11:12 +0700

    Tambah icon toolbar di dalam menu

--- app/src/main/java/edu/stts/materialcomponents/activity/menu11/Menu11Activity.kt
+++ app/src/main/java/edu/stts/materialcomponents/activity/menu11/Menu11Activity.kt
@@ -1,18 +1,57 @@
 package edu.stts.materialcomponents.activity.menu11
 
 import android.os.Bundle
+import android.support.design.widget.NavigationView
+import android.support.v4.view.GravityCompat
+import android.support.v7.app.ActionBarDrawerToggle
 import android.support.v7.app.AppCompatActivity
+import android.view.Gravity
+import android.view.MenuItem
 import edu.stts.materialcomponents.R
+import edu.stts.materialcomponents.utils.Tools
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_menu_11_toolbar.*
+import kotlinx.android.synthetic.main.toolbar_main.*
 
-class Menu11Activity: AppCompatActivity() {
-
+class Menu11Activity: AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_menu_11)
         init()
+        Tools.setSystemBarColor(this, R.color.grey_1000)
     }
 
     private fun init() {
+        setupToolbar()
+        setupNavView()
+    }
+
+    private fun setupToolbar() {
+        setSupportActionBar(toolbarmenu)
+        val actionBar = supportActionBar
+        actionBar?.title = ""
+    }
+
+    private fun setupNavView() {
+        val toggle = ActionBarDrawerToggle(this, drawerLayout, toolbarmenu, R.string.navigation_drawer_open, R.string.navigation_drawer_close)
+        toggle.isDrawerIndicatorEnabled = false
+        toggle.setHomeAsUpIndicator(R.drawable.ic_menu)
+        toggle.syncState()
+        toggle.setToolbarNavigationClickListener { drawerLayout.openDrawer(GravityCompat.START) }
+        navView.menu.clear()
+        navView.inflateMenu(R.menu.activity_menu_11_menu)
+        navView.itemIconTintList = null
+        navView.setNavigationItemSelectedListener(this)
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        drawerLayout.closeDrawers()
+        when (item.itemId) {
+            android.R.id.home -> if (drawerLayout.isDrawerOpen(Gravity.START)) {
+                drawerLayout.closeDrawer(Gravity.START)
+            } else drawerLayout.openDrawer(Gravity.START)
+        }
+        return true
     }
 
 }
