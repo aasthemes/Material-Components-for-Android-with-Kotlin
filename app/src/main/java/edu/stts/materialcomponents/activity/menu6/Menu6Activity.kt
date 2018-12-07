60aae967e95aebf42ad01b44304e2d8c82100985
Aloysius Heinrich <yuliusrizky007@gmail.com>
2018-12-08 03:32:17 +0700

    Button Menu 5 fitur

--- app/src/main/java/edu/stts/materialcomponents/activity/menu6/Menu6Activity.kt
+++ app/src/main/java/edu/stts/materialcomponents/activity/menu6/Menu6Activity.kt
@@ -1,8 +1,11 @@
 package edu.stts.materialcomponents.activity.menu6
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.View
 import edu.stts.materialcomponents.R
+import kotlinx.android.synthetic.main.activity_menu_6.*
 
 class Menu6Activity: AppCompatActivity() {
 
@@ -10,11 +13,17 @@ class Menu6Activity: AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_menu_6)
         init()
-
-
     }
 
     private fun init() {
+
+        buttonSlider.setOnClickListener(View.OnClickListener {
+
+            val i = Intent(this, Menu6SliderActivity::class.java)
+            startActivity(i)
+
+        })
+
     }
 
 }
---
+++ app/src/main/java/edu/stts/materialcomponents/activity/menu6/Menu6SliderActivity.kt
@@ -0,0 +1,18 @@
+package edu.stts.materialcomponents.activity.menu6
+
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import edu.stts.materialcomponents.R
+
+class Menu6SliderActivity: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_menu_6_basic_button)
+        init()
+    }
+
+    private fun init() {
+
+    }
+}
