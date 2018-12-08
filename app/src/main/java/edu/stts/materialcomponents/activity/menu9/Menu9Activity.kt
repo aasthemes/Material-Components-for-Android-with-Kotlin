78f81d4aabeb44043931aee7699cbb4e681515ea
Aloysius Heinrich <yuliusrizky007@gmail.com>
2018-12-09 01:21:51 +0700

    Update bottom navigation view icon + text

--- app/src/main/java/edu/stts/materialcomponents/activity/menu9/Menu9Activity.kt
+++ app/src/main/java/edu/stts/materialcomponents/activity/menu9/Menu9Activity.kt
@@ -5,6 +5,10 @@ import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import edu.stts.materialcomponents.R
 import kotlinx.android.synthetic.main.activity_menu_9.*
+import android.support.design.widget.CoordinatorLayout
+import android.support.design.widget.BottomNavigationView
+
+
 
 class Menu9Activity: AppCompatActivity() {
 
@@ -18,8 +22,16 @@ class Menu9Activity: AppCompatActivity() {
 
     private fun init() {
         button1.setOnClickListener {
-            setContentView(R.layout.activity_menu_9_style)
+            setContentView(R.layout.activity_menu_9_style_text)
         }
+        button2.setOnClickListener {
+            setContentView(R.layout.activity_menu_9_style_icon)
+        }
+        button3.setOnClickListener {
+            setContentView(R.layout.activity_menu_9_transition)
+
+        }
+
     }
 
 }
