1a6cafdd3720bed47cca28d2279a3068a2f9325e
stanislauspn <stanislaus789@gmail.com>
2018-12-09 21:25:25 +0700

    Tambahan file Activity dan file layout

--- app/src/main/java/edu/stts/materialcomponents/activity/menu15/Menu15Activity.kt
+++ app/src/main/java/edu/stts/materialcomponents/activity/menu15/Menu15Activity.kt
@@ -1,8 +1,12 @@
 package edu.stts.materialcomponents.activity.menu15
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.View
+import android.widget.Button
 import edu.stts.materialcomponents.R
+import edu.stts.materialcomponents.activity.menu15.TabBasic.MainActivity
 
 class Menu15Activity: AppCompatActivity() {
 
@@ -10,6 +14,34 @@ class Menu15Activity: AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_menu_15)
         init()
+
+        val button1 = findViewById<Button>(R.id.button1) as Button
+        val button2 = findViewById<Button>(R.id.button2) as Button
+        val button3 = findViewById<Button>(R.id.button3) as Button
+        val button4 = findViewById<Button>(R.id.button4) as Button
+        val button5 = findViewById<Button>(R.id.button5) as Button
+
+
+//        button1.setOnClickListener{
+//            val intent = Intent(context, MainActivity::class.java)
+//            startActivity(intent);
+//        }
+//        button2.setOnClickListener{
+//            val intent = Intent(context, edu.stts.materialcomponents.activity.menu15.TabAndScrolling.MainActivity::class.java)
+//            startActivity(intent);
+//        }
+//        button3.setOnClickListener{
+//            val intent = Intent(context, edu.stts.materialcomponents.activity.menu15.TabWithIcon.MainActivity::class.java)
+//            startActivity(intent);
+//        }
+//        button4.setOnClickListener{
+//            val intent = Intent(context, edu.stts.materialcomponents.activity.menu15.TabWithText.MainActivity::class.java)
+//            startActivity(intent);
+//        }
+//        button5.setOnClickListener{
+//            val intent = Intent(context, edu.stts.materialcomponents.activity.menu15.TabWithIconAndText.MainActivity::class.java)
+//            startActivity(intent);
+//        }
     }
 
     private fun init() {
---
+++ app/src/main/java/edu/stts/materialcomponents/activity/menu15/TabBasic/MyPagerAdapter.kt
@@ -0,0 +1,40 @@
+package edu.stts.materialcomponents.activity.menu15.TabBasic
+
+import android.support.v4.app.Fragment
+import android.support.v4.app.FragmentManager
+import android.support.v4.app.FragmentPagerAdapter
+
+class MyPagerAdapter(fm: FragmentManager): FragmentPagerAdapter(fm) {
+    // sebuah list yang menampung objek Fragment
+//    private val pages = listOf(
+//        FirstFragment(),
+//        SecondFragment(),
+//        ThridFragment()
+//    )
+
+    override fun getItem(position: Int): Fragment {
+        return when(position) {
+            0 -> {
+                FirstFragment()
+            }
+            1 -> SecondFragment()
+            else -> {
+                return ThirdFragment()
+
+            }
+        }
+    }
+
+    override fun getCount(): Int {
+        return 3
+    }
+
+
+    override fun getPageTitle(position: Int): CharSequence? {
+        return when(position){
+            0 -> "First Tab"
+            1 -> "Second Tab"
+            else -> "Third Tab"
+        }
+    }
+}
