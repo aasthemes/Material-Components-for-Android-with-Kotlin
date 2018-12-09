aacb4d119bab859c6e6e6cf86c3ebf517ff85eeb
stanislauspn <stanislaus789@gmail.com>
2018-12-09 22:14:53 +0700

    Tambahan isi fragment dan activity

--- app/src/main/java/edu/stts/materialcomponents/activity/menu15/TabWithIcon/MainActivity.kt
+++ app/src/main/java/edu/stts/materialcomponents/activity/menu15/TabWithIcon/MainActivity.kt
@@ -1,4 +1,16 @@
 package edu.stts.materialcomponents.activity.menu15.TabWithIcon
 
-class MainActivity {
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import edu.stts.materialcomponents.R
+import kotlinx.android.synthetic.main.activity_menu_15_tabwithicon.*
+
+class MainActivity: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        viewpager_main.adapter = MyPagerAdapter(supportFragmentManager)
+        tabs_main.setupWithViewPager(viewpager_main)
+    }
 }
