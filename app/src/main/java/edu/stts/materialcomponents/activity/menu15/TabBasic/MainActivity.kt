f822ff461afbf70f610bd4f46d6e6be0ae071ac1
stanislauspn <stanislaus789@gmail.com>
2018-12-09 22:28:25 +0700

    Tambah Coding

--- app/src/main/java/edu/stts/materialcomponents/activity/menu15/TabBasic/MainActivity.kt
+++ app/src/main/java/edu/stts/materialcomponents/activity/menu15/TabBasic/MainActivity.kt
@@ -6,11 +6,13 @@ import edu.stts.materialcomponents.R
 import kotlinx.android.synthetic.main.activity_menu_15_basic.*
 
 class MainActivity: AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        override fun onCreate(savedInstanceState: Bundle?) {
+            super.onCreate(savedInstanceState)
+            setContentView(R.layout.activity_main)
 
-        viewpager_main.adapter = MyPagerAdapter(supportFragmentManager)
-        tabs_main.setupWithViewPager(viewpager_main)
-    }
+            viewpager_main.adapter = MyPagerAdapter(supportFragmentManager)
+            tabs_main.setupWithViewPager(viewpager_main)
+        }
+
+    //tambahmain activity
 }
