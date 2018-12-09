9465de4c1c37bfe439d59f6478387924f8397a99
Danar <donodanar35@gmail.com>
2018-12-09 09:01:02 +0700

    fix style

--- app/src/main/java/edu/stts/materialcomponents/activity/menu7/myStyleProgressBar.kt
+++ app/src/main/java/edu/stts/materialcomponents/activity/menu7/myStyleProgressBar.kt
@@ -1,8 +1,10 @@
 package edu.stts.materialcomponents.activity.menu7
 
+import android.app.ProgressDialog
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.ProgressBar
 import edu.stts.materialcomponents.R
 import kotlinx.android.synthetic.main.activity_my_style_progress_bar.*
 import kotlin.system.exitProcess
@@ -13,32 +15,52 @@ class myStyleProgressBar : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_my_style_progress_bar)
 
+        progressBarHorizontal.visibility = View.INVISIBLE
+        progressBarCircle.visibility = View.INVISIBLE
+        var progress: Int = 0
+        var myProgress: ProgressBar = progressBarHorizontal
         buttonProcess.setOnClickListener(){
-            if(radioButtonCicle.isChecked){
-                progressBarCircle.visibility = android.view.View.VISIBLE
-                var i=0;
-                while(i<Int.MAX_VALUE){
-                    i++
-                    progressBarCircle.progress = progressBarCircle.progress + 10
-                    textViewProgress.text = "Progress : $i%"
-                    if(progressBarCircle.progress == 100){
-                        progressBarCircle.visibility = android.view.View.GONE
-                        exitProcess(1)
-                    }
-                }
-            }else if(radioButtonHorizontal.isChecked){
-                progressBarHorizontal.visibility = android.view.View.VISIBLE
-                var i=0;
-                while(i<Int.MAX_VALUE){
-                    i++
-                    progressBarHorizontal.progress  = progressBarHorizontal.progress + 10
-                    textViewProgress.text = "Progress : $i%"
-                    if(progressBarHorizontal.progress == 100){
-                        progressBarHorizontal.visibility = android.view.View.GONE
-                        exitProcess(1)
-                    }
-                }
+            if(radioButtonHorizontal.isChecked){
+                progressBarHorizontal.visibility = View.VISIBLE
+                progressBarCircle.visibility = View.INVISIBLE
+            }else if(radioButtonCicle.isChecked){
+                progressBarCircle.visibility = View.VISIBLE
+                progressBarHorizontal.visibility = View.INVISIBLE
             }
+
+            if(progressBarHorizontal.visibility == View.VISIBLE){
+                myProgress = progressBarHorizontal
+                myProgress.max = 100
+            }else if(progressBarCircle.visibility == View.VISIBLE){
+                myProgress = progressBarCircle
+                myProgress.max = 100
+            }
+            setProgressValue(progress,myProgress)
         }
     }
+
+    public fun setProgressValue(progress: Int, myProgress: ProgressBar){
+
+        Thread(Runnable {
+            @Override
+            fun run(){
+                try {
+                    //Thread.sleep(10)
+                    myProgress.setProgress(progress)
+                    toast(" Harap tunggu: $progress%")
+
+                    if(myProgress.progress != 100){
+                        setProgressValue(progress + 10,myProgress)
+                    }else{
+                        myProgress.visibility = View.GONE
+                        toast("sukses")
+                    }
+                }   catch (e : Exception){
+                    toast(e.toString())
+                }
+
+            }
+        }).start()
+    }
 }
+
