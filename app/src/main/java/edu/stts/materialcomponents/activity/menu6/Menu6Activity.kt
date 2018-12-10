d6fc1b741cc3ba2cbaf45528ae2b913190f5dc46
Aloysius Heinrich <yuliusrizky007@gmail.com>
2018-12-11 01:01:32 +0700

    Circular / Circle View

--- app/src/main/java/edu/stts/materialcomponents/activity/menu6/Menu6Activity.kt
+++ app/src/main/java/edu/stts/materialcomponents/activity/menu6/Menu6Activity.kt
@@ -4,7 +4,9 @@ import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.view.View
+import android.widget.ImageView
 import edu.stts.materialcomponents.R
+import kotlinx.android.synthetic.*
 import kotlinx.android.synthetic.main.activity_menu_6.*
 
 class Menu6Activity: AppCompatActivity() {
@@ -12,17 +14,25 @@ class Menu6Activity: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_menu_6)
+
+
         init()
     }
 
     private fun init() {
 
-        buttonSlider.setOnClickListener(View.OnClickListener {
+        buttonSlider.setOnClickListener{
+            setContentView(R.layout.activity_menu_6_slider_view)
+        }
 
-            val i = Intent(this, Menu6SliderActivity::class.java)
-            startActivity(i)
+        buttonRounded.setOnClickListener{
+            setContentView(R.layout.activity_menu_6_rounded_view)
+        }
 
-        })
+        buttonCircular.setOnClickListener{
+            setContentView(R.layout.activity_menu_6_circular_view)
+
+        }
 
     }
 
