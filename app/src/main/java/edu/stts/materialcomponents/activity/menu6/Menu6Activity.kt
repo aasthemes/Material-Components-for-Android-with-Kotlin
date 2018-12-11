fc45abf9915ebda369a5c7c7cf58c2160abe47f2
Aloysius Heinrich <yuliusrizky007@gmail.com>
2018-12-11 19:01:24 +0700

    Zoom In and Zoom Out Fix

--- app/src/main/java/edu/stts/materialcomponents/activity/menu6/Menu6Activity.kt
+++ app/src/main/java/edu/stts/materialcomponents/activity/menu6/Menu6Activity.kt
@@ -1,25 +1,36 @@
 package edu.stts.materialcomponents.activity.menu6
 
-import android.content.Intent
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.support.constraint.ConstraintLayout
 import android.support.v7.app.AppCompatActivity
 import android.view.View
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
 import android.widget.ImageView
 import edu.stts.materialcomponents.R
-import kotlinx.android.synthetic.*
 import kotlinx.android.synthetic.main.activity_menu_6.*
+import kotlinx.android.synthetic.main.activity_menu_6_zoominzoomout.*
+import kotlinx.android.synthetic.main.activity_menu_6_zoominzoomout.view.*
+import com.github.chrisbanes.photoview.PhotoView
+
+
+
 
 class Menu6Activity: AppCompatActivity() {
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_menu_6)
 
-
         init()
     }
 
+    @SuppressLint("WrongViewCast")
     private fun init() {
+
 
         buttonSlider.setOnClickListener{
             setContentView(R.layout.activity_menu_6_slider_view)
@@ -32,6 +43,13 @@ class Menu6Activity: AppCompatActivity() {
         buttonCircular.setOnClickListener{
             setContentView(R.layout.activity_menu_6_circular_view)
 
+        }
+
+        buttonZoom.setOnClickListener{
+            setContentView(R.layout.activity_menu_6_zoom_view)
+
+            val photoView = findViewById<View>(R.id.photo_view) as PhotoView
+            photoView.setImageResource(R.drawable.logo)
         }
 
     }
