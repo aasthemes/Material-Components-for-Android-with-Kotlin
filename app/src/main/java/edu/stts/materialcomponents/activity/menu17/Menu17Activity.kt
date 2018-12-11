cd17d95cbcfb2047b039bd590eb63098e8cfb099
Cornelius FN <javablackrose@gmail.com>
2018-12-11 17:20:27 +0700

    Merge branch 'team17' of C:\Users\Cornelius FN\StudioProjects\Material-Components-for-Android-with-Kotlin with conflicts.

--- app/src/main/java/edu/stts/materialcomponents/activity/menu17/Menu17Activity.kt
+++ app/src/main/java/edu/stts/materialcomponents/activity/menu17/Menu17Activity.kt
@@ -1,10 +1,12 @@
 package edu.stts.materialcomponents.activity.menu17
 
-import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import edu.stts.materialcomponents.R
-import kotlinx.android.synthetic.main.activity_menu_17.*
+import edu.stts.materialcomponents.activity.MainActivity
+import android.content.Intent
+import android.view.View
+
 
 class Menu17Activity: AppCompatActivity() {
 
@@ -15,25 +17,6 @@ class Menu17Activity: AppCompatActivity() {
     }
 
     private fun init() {
-        menu_17_btn_popup.setOnClickListener {
-            val intent = Intent(this, PopupMenu::class.java)
-            startActivity(intent)
-        }
 
-        menu_17_btn_icon_label.setOnClickListener {
-            val intent = Intent(this, PopupLabel::class.java)
-            startActivity(intent)
-        }
-
-        menu_17_btn_position.setOnClickListener {
-            val intent = Intent(this, BasicWithCustomPosition::class.java)
-            startActivity(intent)
-        }
-
-        menu_17_btn_stroke.setOnClickListener {
-            val intent = Intent(this, StrokeLoading::class.java)
-            startActivity(intent)
-        }
     }
-
 }
