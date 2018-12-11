d3fce6e5fdd816a699ae4cc28a5df22d4ea1a773
Cornelius FN <javablackrose@gmail.com>
2018-12-11 18:05:30 +0700

    Menu link, FAB Color

--- app/src/main/java/edu/stts/materialcomponents/activity/menu17/Menu17Activity.kt
+++ app/src/main/java/edu/stts/materialcomponents/activity/menu17/Menu17Activity.kt
@@ -3,9 +3,10 @@ package edu.stts.materialcomponents.activity.menu17
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import edu.stts.materialcomponents.R
-import edu.stts.materialcomponents.activity.MainActivity
 import android.content.Intent
 import android.view.View
+import edu.stts.materialcomponents.R.id.menu_17_btn_color
+import kotlinx.android.synthetic.main.activity_menu_17.*
 
 
 class Menu17Activity: AppCompatActivity() {
@@ -17,6 +18,35 @@ class Menu17Activity: AppCompatActivity() {
     }
 
     private fun init() {
+        menu_17_btn_position.setOnClickListener(View.OnClickListener {
 
+            val i = Intent(this, BasicWithCustomPosition::class.java)
+            startActivity(i)
+
+        })
+        menu_17_btn_popup.setOnClickListener(View.OnClickListener {
+
+            val i = Intent(this, PopupMenu::class.java)
+            startActivity(i)
+
+        })
+        menu_17_btn_stroke.setOnClickListener(View.OnClickListener {
+
+            val i = Intent(this, StrokeLoading::class.java)
+            startActivity(i)
+
+        })
+        menu_17_btn_icon_label.setOnClickListener(View.OnClickListener {
+
+            val i = Intent(this, PopupLabel::class.java)
+            startActivity(i)
+
+        })
+        menu_17_btn_color.setOnClickListener(View.OnClickListener {
+
+            val i = Intent(this, BackgroundColor::class.java)
+            startActivity(i)
+
+        })
     }
 }
