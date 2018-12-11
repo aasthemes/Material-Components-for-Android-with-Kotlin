dcd1bd4c6984565f0865c3824193decdf1dee3e4
Imamul Akhyar <khen.imvu@gmail.com>
2018-12-11 19:18:24 +0700

    Background color

--- app/src/main/java/edu/stts/materialcomponents/activity/menu17/BackgroundColor.kt
+++ app/src/main/java/edu/stts/materialcomponents/activity/menu17/BackgroundColor.kt
@@ -1,8 +1,10 @@
 package edu.stts.materialcomponents.activity.menu17
 
+import android.content.res.ColorStateList
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.design.widget.Snackbar
+import android.support.v4.content.ContextCompat
 import android.view.View
 import edu.stts.materialcomponents.R
 import kotlinx.android.synthetic.main.activity_menu_17_background_color.*
@@ -12,11 +14,9 @@ class BackgroundColor : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_menu_17_background_color)
-//        val fab: View = findViewById(R.id.floatingActionButton2)
-//        fab.setOnClickListener {
-//            floatingActionButton2.setBackgroundColor(Color.BLUE)
-//        }
-        var bm:
+        floatingActionButton2.setOnClickListener {
+            floatingActionButton2.setBackgroundTintList(ColorStateList.valueOf(ContextCompat.getColor(this,R.color.red_300)))
+        }
     }
 
 }
