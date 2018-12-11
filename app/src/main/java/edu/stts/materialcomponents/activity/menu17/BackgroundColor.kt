76585cd3a00452ea6b5beaff6120f892762f3950
Cornelius FN <javablackrose@gmail.com>
2018-12-11 18:27:16 +0700

    Menu link, FAB Color2

---
+++ app/src/main/java/edu/stts/materialcomponents/activity/menu17/BackgroundColor.kt
@@ -0,0 +1,22 @@
+package edu.stts.materialcomponents.activity.menu17
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.support.design.widget.Snackbar
+import android.view.View
+import edu.stts.materialcomponents.R
+import kotlinx.android.synthetic.main.activity_menu_17_background_color.*
+
+class BackgroundColor : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_menu_17_background_color)
+//        val fab: View = findViewById(R.id.floatingActionButton2)
+//        fab.setOnClickListener {
+//            floatingActionButton2.setBackgroundColor(Color.BLUE)
+//        }
+        var bm:
+    }
+
+}
