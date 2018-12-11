a8e4c923ca5855a9eea40ea2d91864ece899f67f
Yohanes Wisnu Perdana <perdanayohanes43@gmail.com>
2018-12-11 13:34:50 +0700

    button menu checkbox dataadapter

--- app/src/main/java/edu/stts/materialcomponents/activity/menu12/Menu12Activity.kt
+++ app/src/main/java/edu/stts/materialcomponents/activity/menu12/Menu12Activity.kt
@@ -4,6 +4,8 @@ import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.view.View
+import android.widget.Button
+import android.widget.ListView
 import edu.stts.materialcomponents.R
 import kotlinx.android.synthetic.main.activity_menu_2.*
 
@@ -16,13 +18,23 @@ class Menu12Activity: AppCompatActivity() {
     }
 
     private fun init() {
-        button1.setOnClickListener(View.OnClickListener {
-            val i = Intent(this, ChangeColorActivity::class.java)
-            startActivity(i)
-        })
 
-        button2.setOnClickListener(View.OnClickListener {
-        })
+        fun onCreate(savedInstanceState: Bundle?) {
+
+            button3.setOnClickListener {
+                val i = Intent(this, CheckboxWithAdapter::class.java)
+                startActivity(i)
+            }
+
+            button1.setOnClickListener(View.OnClickListener {
+                val i = Intent(this, ChangeColorActivity::class.java)
+                startActivity(i)
+            })
+
+            button2.setOnClickListener(View.OnClickListener {
+            })
+
+        }
     }
 
 }
