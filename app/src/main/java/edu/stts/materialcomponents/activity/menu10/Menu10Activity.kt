9ea9257ae337d6e69b3b3366204de6bc80e25f3a
Garry Laly <garrylaly@gmail.com>
2018-12-14 07:50:48 +0700

    Merge remote-tracking branch 'origin/team10' into development

--- app/src/main/java/edu/stts/materialcomponents/activity/menu10/Menu10Activity.kt
+++ app/src/main/java/edu/stts/materialcomponents/activity/menu10/Menu10Activity.kt
@@ -2,14 +2,26 @@ package edu.stts.materialcomponents.activity.menu10
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
 import edu.stts.materialcomponents.R
+
+
 
 class Menu10Activity: AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_menu_10)
-        init()
+//        ERROR, dikomen oleh team master
+//        val showToast = findViewById(R.id.showToast) as Button
+//        showToast.setOnClickListener(object : View.OnClickListener() {
+//            fun onClick(v: View) {
+//                Toast.makeText(this@Menu10Activity, "Toast Success", Toast.LENGTH_SHORT).show()
+//            }
+//        })
+
     }
 
     private fun init() {
